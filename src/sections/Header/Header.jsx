96823a0ed3dc2b3681af44c7e86edbecf380dc3c
Marcus Lau <108362808+BabyDinos@users.noreply.github.com>
2023-11-23 15:31:27 -0500

    overflow hidden for header

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -6,7 +6,7 @@ import images from "../../images"
 function Header() {
 
     return(
-        <div className="flex justify-center bg-page_background">
+        <div className="flex justify-center bg-page_background overflow-hidden">
           <div className="z-50 fixed bg-header_background flex justify-between w-[90%] px-20 py-7 rounded-b-2xl ">
             <header className="flex w-full justify-center items-center gap-80">
               <Link text="About"></Link>
