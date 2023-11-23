9f3e78cd8e26caf4560dfe3f82e8cd1bf94e3f2e
Marcus Lau <108362808+BabyDinos@users.noreply.github.com>
2023-11-23 16:56:36 -0500

    Made a smaller gap in header for laptop-size screens

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
     return(
         <div className="flex justify-center bg-page_background">
           <div className="z-50 fixed bg-header_background flex justify-between w-[90%] px-20 py-7 rounded-b-2xl">
-            <header className="flex w-full justify-center items-center gap-4 lg:gap-80">
+            <header className="flex w-full justify-center items-center gap-4 md:gap-40 lg:gap-72 2xl:gap-80">
               <Link text="About"></Link>
               <Link text="Experience"></Link>
               <Link text="Projects"></Link>
